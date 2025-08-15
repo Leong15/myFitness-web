1eec7d11e70cf8a1fd191726b586f00d16ec6b2c
Leong15 <53085228+Leong15@users.noreply.github.com>
2025-08-15 17:36:01 +0800

    change UI of menubar and bg. Add component for searchbybarcode

--- share/component/login_panel/login_panel.tsx
+++ share/component/login_panel/login_panel.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import axios from "axios";
+// import axios from "axios";
 import { Button } from "primereact/button";
 import { InputText } from 'primereact/inputtext';
 import { Panel } from 'primereact/panel';
@@ -16,7 +16,7 @@ import { useRouter } from "next/navigation";
 /* const endpoint = "http://localhost:3000"; */
 
 function Login_Panel() {
-  const toast = useRef(null);
+  const toast = useRef<Toast>(null);
   const [forgetDialogVisible, setForgetDialogVisible] = useState(false);
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
@@ -24,21 +24,21 @@ function Login_Panel() {
   const [loginStaff,setLoginStaff] = useState('');
 
   const show = () => {
-    toast.current.show({
+    toast.current?.show({
       severity: "error",
       summary: "Wrong Username or Password",
-      detail: form.getValues("value"),
+      detail: form.getValues("username"),
     })
   };
 
   const forgetShow = () => {
-    toast.current.show({
+    toast.current?.show({
       severity: "success",
       summary: "New Password will sent to your email",
     })
   };
   const forgetErrorShow = () => {
-    toast.current.show({
+    toast.current?.show({
       severity: "error",
       summary: "Account not found",
     })
@@ -74,12 +74,12 @@ function Login_Panel() {
   } */
   
 
-  const defaultValues = { value: "" };
-  const form = useForm(defaultValues);
+  const defaultValues = { username: "", password: "" };
+  const form = useForm({ defaultValues });
   const errors = form.formState.errors;
   const router = useRouter();
 
-  const handleSubmit = async e => {
+  const handleSubmit = async (e: { username: any; password: any; }) => {
     const username = e.username;
     const password = e.password;
     console.log(e)
--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,14 +1,18 @@
+import React, {ReactNode} from 'react';
 import CustomMenubar from "../../../share/component/Menubar/CustomMenubar";
+import '../../../share/css/menubar.css'
 
-export default function DashLayout ({
-    children
-}:{
-    children:React.ReactNode
-}){
-    return(
-        <>
-            <CustomMenubar />
-            {children}
-        </>
-    )
+interface LayoutProps{
+    children: ReactNode;
 }
+
+const ProtectedLayout:React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <header className="fixed-header"><CustomMenubar /></header>
+      <main style={{paddingTop:'100px'}}>{children}</main>
+    </>
+  );
+};
+
+export default ProtectedLayout;
